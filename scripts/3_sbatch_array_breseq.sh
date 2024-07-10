ea8577fb243b90d57ef1fc7687621022e5cc2eee
soom12 <soo.m@northeastern.edu>
2024-07-10 11:15:52 -0400

    adjusted script contents for current project (directories, single end seq, etc)

--- scripts/3_sbatch_array_breseq.sh
+++ scripts/3_sbatch_array_breseq.sh
@@ -5,12 +5,10 @@
 #SBATCH --array=1-11%12
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=100G
-#SBATCH --output=/work/geisingerlab/Mark/breseq/2024-05-21_breseq/logs/%x_%A_%a.out
-#SBATCH --error=/work/geisingerlab/Mark/breseq/2024-05-21_breseq/logs/%x_%A_%a.err
+#SBATCH --output=/work/geisingerlab/Mark/breseq/2024-07-09_breseq_nicole-5075-muts/logs/%x_%A_%a.out
+#SBATCH --error=/work/geisingerlab/Mark/breseq/2024-07-09_breseq_nicole-5075-muts/logs/%x_%A_%a.err
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=soo.m@northeastern.edu
-
-## MWS May 21, 2024 updated
 
 source ./config.cfg
 
@@ -33,9 +31,8 @@ mkdir -p $BRESEQ_OUTPATH
 
 name=`sed -n "$SLURM_ARRAY_TASK_ID"p $SAMPLE_SHEET |  awk '{print $1}'`
 r1=`sed -n "$SLURM_ARRAY_TASK_ID"p $SAMPLE_SHEET |  awk '{print $2}'`
-r2=`sed -n "$SLURM_ARRAY_TASK_ID"p $SAMPLE_SHEET |  awk '{print $3}'`
 
-echo "Running breseq on files $r1 and $r2"
-breseq -r ${REFERENCE_CHR} -r ${REFERENCE_PAB1} -r ${REFERENCE_PAB2} -r ${REFERENCE_PAB3} -o ${BRESEQ_OUTPATH}/${name} ${r1} ${r2}
+echo "Running breseq on file $r1"
+breseq -r ${REFERENCE_CHR} -r ${REFERENCE_PAB1} -r ${REFERENCE_PAB2} -r ${REFERENCE_PAB3} -o ${BRESEQ_OUTPATH}/${name} ${r1}
 
 echo "breseq script complete $(date)"
