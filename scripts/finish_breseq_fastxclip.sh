d44523093672a76c59ed6653952f1bab6c275036
soom12 <soo.m@northeastern.edu>
2024-07-22 12:47:06 -0400

    array script to finish breseq

--- scripts/finish_breseq_fastxclip.sh
+++ scripts/finish_breseq_fastxclip.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --partition=short
-#SBATCH --job-name=breseq
-#SBATCH --time=04:00:00
-#SBATCH -N 2
-#SBATCH -n 2
-#SBATCH --mem=200G
+#SBATCH --job-name=finish-breseq-fastx
+#SBATCH --time=08:00:00
+#SBATCH --array=1-25%5
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=400G
 #SBATCH --output=/work/geisingerlab/Mark/breseq/2024-07-09_breseq_nicole-5075-muts/logs/%x_%y.out
 #SBATCH --error=/work/geisingerlab/Mark/breseq/2024-07-09_breseq_nicole-5075-muts/logs/%x_%y.err
 #SBATCH --mail-type=END,FAIL
@@ -16,9 +16,11 @@ module load anaconda3/2021.05
 eval "$(conda shell.bash hook)"
 conda activate /work/geisingerlab/conda_env/breseq_env
 
-breseq -r ${REFERENCE_CHR} -r ${REFERENCE_PAB1} -r ${REFERENCE_PAB2} -r ${REFERENCE_PAB3} -o output/breseq/trimmed_fastxclip/NRA277 ./input/fastq/trimmed_fastxclipper_egadapter/NRA275_S168_L008_R1_001_trimmed.fastq
-breseq -r ${REFERENCE_CHR} -r ${REFERENCE_PAB1} -r ${REFERENCE_PAB2} -r ${REFERENCE_PAB3} -o output/breseq/trimmed_fastxclip/NRA295 ./input/fastq/trimmed_fastxclipper_egadapter/NRA295_S105_L008_R1_001_trimmed.fastq
-breseq -r ${REFERENCE_CHR} -r ${REFERENCE_PAB1} -r ${REFERENCE_PAB2} -r ${REFERENCE_PAB3} -o output/breseq/trimmed_fastxclip/NRA296 ./input/fastq/trimmed_fastxclipper_egadapter/NRA296_S106_L008_R1_001_trimmed.fastq
-breseq -r ${REFERENCE_CHR} -r ${REFERENCE_PAB1} -r ${REFERENCE_PAB2} -r ${REFERENCE_PAB3} -o output/breseq/trimmed_fastxclip/NRA297 ./input/fastq/trimmed_fastxclipper_egadapter/NRA297_S107_L008_R1_001_trimmed.fastq
-breseq -r ${REFERENCE_CHR} -r ${REFERENCE_PAB1} -r ${REFERENCE_PAB2} -r ${REFERENCE_PAB3} -o output/breseq/trimmed_fastxclip/NRA299 ./input/fastq/trimmed_fastxclipper_egadapter/NRA299_S109_L008_R1_001_trimmed.fastq
+unfinished_samples_array=(276 280 281 282 283 285 287 289 290 293 294 298 304 306 308 309 310 311 313 314 319 331 342 345 351)
 
+echo "Reference genome files: " $REFERENCE_CHR $REFERENCE_PAB1 $REFERENCE_PAB2 $REFERENCE_PAB3
+echo "Breseq output saved to: " $BRESEQ_OUTPATH/breseq-fastx
+
+mkdir -p $BRESEQ_OUTPATH/breseq-fastx
+
+echo breseq -r ${REFERENCE_CHR} -r ${REFERENCE_PAB1} -r ${REFERENCE_PAB2} -r ${REFERENCE_PAB3} -o $BRESEQ_OUTPATH/breseq-fastx/NRA${unfinished_samples_array[$"$SLURM_ARRAY_TASK_ID"]} /work/geisingerlab/Mark/breseq/2024-07-09_breseq_nicole-5075-muts/input/fastq/trimmed_fastxclip/NRA${unfinished_samples_array[$"$SLURM_ARRAY_TASK_ID"]}_S167_L008_R1_001_fastxclipper_trimmed.fastq
